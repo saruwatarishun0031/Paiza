39b08073f1608314e89e8b65c54aac7176c277b6
SHUNSHUN-0031 <saruwatarishun15@gmail.com>
2024-05-11 18:12:17 +0900

    [Add]砲丸投げ

---
+++ C142/Program.cs
@@ -0,0 +1,32 @@
+﻿namespace C142
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            bool trye = false;
+            var line = Console.ReadLine();
+            var line2 = Console.ReadLine();
+            int y = int.Parse(line2);
+            var line3 = Console.ReadLine().Split(' ');
+            for (int i = 0; i < line3.Length; i++)
+            {
+                if (line3[i] == line)
+                {
+                    trye = true;
+                }
+                
+            }
+            if (trye == true)
+            {
+                Console.WriteLine("Yes");
+            }
+            else
+            {
+                Console.WriteLine("No");
+            }
+
+
+        }
+    }
+}
