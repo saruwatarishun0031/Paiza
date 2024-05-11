20c26819a6e1a50226460ba5eb2e8e9ad668f891
SHUNSHUN-0031 <saruwatarishun15@gmail.com>
2024-05-11 14:52:57 +0900

    [add]配列（リスト）データの足し合わせ

---
+++ Addingarraydata/Program.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections.Generic;
+namespace Addingarraydata
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            List<int> ary = new List<int>() { 4, 0, 5, -1, 3, 10, 6, -8 };
+            int value = 0;
+            for (int i = 0; i < ary.Count; i++)
+            {
+                if (ary[i] >= 5)
+                {
+                    value += ary[i];
+                }
+            }
+            Console.WriteLine(value);
+        }
+    }
+}
