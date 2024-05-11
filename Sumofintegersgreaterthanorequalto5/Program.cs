17ed8bfc4b01a7ab0ff4b55abc9eb3a71621c2c3
SHUNSHUN-0031 <saruwatarishun15@gmail.com>
2024-05-11 15:13:01 +0900

    [add]5以上の整数の合計

---
+++ Sumofintegersgreaterthanorequalto5/Program.cs
@@ -0,0 +1,21 @@
+﻿namespace Sumofintegersgreaterthanorequalto5
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var line = Console.ReadLine();
+            int n = int.Parse(line);
+            int vecter = 0;
+            for (int i = 0; i < n; i++)
+            {
+                int x = Console.Read();
+                if (x <= 5)
+                {
+                    vecter += i;
+                }
+            }
+            Console.WriteLine(vecter);
+        }
+    }
+}
