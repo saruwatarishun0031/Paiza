ce63ee719f929cd606ed1c57ea29c0415aa18453
SHUNSHUN-0031 <saruwatarishun15@gmail.com>
2024-05-11 18:12:38 +0900

    [Add]レストランのメニュー

---
+++ D254/Program.cs
@@ -0,0 +1,22 @@
+﻿namespace D254
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var line = Console.ReadLine();
+            var line2 = Console.ReadLine();
+            int x = int.Parse(line);
+            int y = int.Parse(line2);
+            if (x < y)
+            {
+                Console.WriteLine(y - x);
+            }
+            else
+            {
+                Console.WriteLine(x - y);
+            }
+
+        }
+    }
+}
