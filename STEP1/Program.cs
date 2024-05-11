22b4d1139abc09a7c7a371679901bebf259da3a6
SHUNSHUN-0031 <saruwatarishun15@gmail.com>
2024-05-11 13:50:15 +0900

    [add]STEP: 1 インクリメント

---
+++ STEP1/Program.cs
@@ -0,0 +1,16 @@
+﻿namespace Increment
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            // 自分の得意な言語で
+            // Let's チャレンジ！！
+            var line = Console.ReadLine();
+            int x = int.Parse(line);
+            Console.WriteLine(x + 1);
+
+        }
+    }
+}
