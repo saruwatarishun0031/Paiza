238ce67feafc08ae237df668f58d4c2cfd7e8122
SHUNSHUN-0031 <saruwatarishun15@gmail.com>
2024-05-11 13:50:43 +0900

    [add]STEP: 2 複数行の入力と出力

---
+++ STEP2/Program.cs
@@ -0,0 +1,20 @@
+﻿namespace MultilineInputAndOutput
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            // 自分の得意な言語で
+            // Let's チャレンジ！！
+            var line = Console.ReadLine();
+            int x = int.Parse(line);
+            for (int i = 0; i < x; i++)
+            {
+                var y = Console.ReadLine();
+                Console.WriteLine(y);
+            }
+
+        }
+    }
+}
