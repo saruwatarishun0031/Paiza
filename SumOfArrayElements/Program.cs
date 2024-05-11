087f6f2f91a8a9504d1eaed3e38012639bb2c8b7
SHUNSHUN-0031 <saruwatarishun15@gmail.com>
2024-05-11 14:08:15 +0900

    [add]配列（リスト）要素の合計

---
+++ SumOfArrayElements/Program.cs
@@ -0,0 +1,18 @@
+﻿using System.Linq;
+namespace SumOfArrayElements
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            List<int> ary = new List<int>() { 1, 3, 5, 6, 3, 2, 5, 23, 2 };
+            int value = 0;
+            for (int i = 0; i < ary.Count; i++)
+            {
+                value += ary[i];
+            }
+            Console.WriteLine(value);
+
+        }
+    }
+}
