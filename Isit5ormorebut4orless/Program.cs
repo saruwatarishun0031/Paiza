711df5cbf69a4a0afecd7f4ad141f78d955c92b5
SHUNSHUN-0031 <saruwatarishun15@gmail.com>
2024-05-11 14:14:00 +0900

    [Add]5以上か4以下か

---
+++ Isit5ormorebut4orless/Program.cs
@@ -0,0 +1,19 @@
+﻿namespace Isit5ormorebut4orless
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var line = Console.ReadLine();
+            int x = int.Parse(line);
+            if (x >= 5)
+            {
+                Console.WriteLine("high");
+            }
+            else
+            {
+                Console.WriteLine("low");
+            }
+        }
+    }
+}
