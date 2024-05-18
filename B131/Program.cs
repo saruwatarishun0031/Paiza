5cd53f053e2faf18b3aedc16a773e91e9990f2a7
SHUNSHUN-0031 <saruwatarishun15@gmail.com>
2024-05-18 16:39:06 +0900

    [Add]【異能コラボ問題】B131:運賃計算

---
+++ B131/Program.cs
@@ -0,0 +1,58 @@
+﻿using System.Collections.Generic;
+using static System.Runtime.InteropServices.JavaScript.JSType;
+
+namespace B131
+{
+    internal class Program
+    {
+        static void Main()
+        {
+
+            var StationN = Array.ConvertAll(Console.ReadLine().Split(), int.Parse);
+            int x = StationN[0];
+            int y = StationN[1];
+            int z = 0;
+            //int w = 0;
+            int d = 0;
+            int ans = 0;
+            List<List<int>> date = new List<List<int>>();
+            for (int i = 0; i < x; i++)
+            {
+                var row = Array.ConvertAll(Console.ReadLine().Split(), int.Parse);
+                date.Add(new List<int>(row));
+            }
+            var Stops =int.Parse(Console.ReadLine());
+            for (int i = 0;i < Stops; i++)
+            {
+                var line = Array.ConvertAll(Console.ReadLine().Split(), int.Parse);
+                int s = line[0];
+                int e = line[1];
+                if (i == 0)
+                {
+                    ans += date[s - 1][e - 1];
+                }
+                else
+                {
+                    if (date[s - 1][z] < date[s - 1][e - 1])
+                    {
+                        d = date[s - 1][e - 1] - date[s - 1][z];
+                        ans += d;
+                    }
+                    else if (date[s - 1][z] > date[s - 1][e - 1])
+                    {
+                        d = date[s - 1][z] - date[s - 1][e - 1];
+                        ans += d;
+                    }
+                    
+                }
+                z = e - 1;
+
+            }
+            Console.WriteLine(ans);
+            //foreach (var row in date)
+            //{
+            //    Console.WriteLine(string.Join(" ", row));
+            //}
+        }
+    }
+}
